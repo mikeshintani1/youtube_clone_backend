8852a42520f97bf98e6da9020bb544d65202ed2f
michael shintani <mikeshintani@yahoo.com>
2022-05-05 14:04:36 -0700

    related videos created and operational

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import VideoPage from "./pages/VideoPage/videoPage";
 import SearchPage from "./components/SearchPage/SearchPage"
+import RelatedVideos from "./components/RelatedVideos/RelatedVideos"
 
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
@@ -27,6 +28,7 @@ function App() {
 
   const [searchVideos, setSearchVideoes] = useState([]);
   const [currentVideoId, setCurrentVideoId] = useState("vrAMRxBB5KI")
+  const [relatedVideoId, setRelatedVideoId] = useState("vrAMRxBB5KI")
   // make hook to save related video results
 
 
@@ -38,6 +40,12 @@ function App() {
     let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${searchTerm}&type=video&key=AIzaSyCjBTtSq2A2IWbTfV0uUN-LMhuBc2UzKM8&part=snippet&maxResults=5`)
     console.log(response.data.items)
     setSearchVideoes(response.data.items)
+  }
+  async function getRelatedVideo(searchTerm = {currentVideoId}){
+    let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${currentVideoId}&type=video&key=AIzaSyCjBTtSq2A2IWbTfV0uUN-LMhuBc2UzKM8`)
+    console.log(response.data.items)
+    setRelatedVideoId(response.data.items)
+  
   }
 // make another axios call for related videos, you will plug in currentVideoId in this one just like you did searchTerm in the above example
 /// then you will need to pass the value saved in the hook above down to a compoent do display the related videos, just like you did with SearchPage
@@ -56,6 +64,7 @@ function App() {
               <SearchBar getSearchVideo={getSearchVideo} />
               <VideoPlayer videoId={currentVideoId}/>
               <SearchPage videos={searchVideos} setId={setCurrentVideoId}/>
+              <RelatedVideos videos={relatedVideoId} setId={setCurrentVideoId}/>
   
   
             </PrivateRoute>
--- frontend/src/components/RelatedVideos/relatedVideos.jsx
+++ frontend/src/components/RelatedVideos/relatedVideos.jsx
@@ -1,1 +1,38 @@
 import {} from 'react-router-dom'
+import React, { useState } from 'react';
+
+
+
+
+const RelatedVideos = (props) => {
+    
+    const [searchVideo, setSearchVideo] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(searchVideo)
+        props.getSearchVideo(searchVideo);
+    }
+  
+    
+
+return (
+    <div className='SearchBar'>
+        <form >
+        <div>
+            <input
+                className='searchVideo'
+                type="text"
+                value={searchVideo}
+                onChange={(e) =>  setSearchVideo(e.target.value)}
+                placeholder="Find Your Video!"
+                />{" "}
+                <button onClick={handleSubmit} className='findvideo' type="submit" >FIND VIDEO</button>
+        </div>
+        </form>
+    </div>
+    
+);
+}
+
+export default RelatedVideos
